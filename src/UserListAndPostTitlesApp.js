e8d30c8319f7cbda3e2ba12d3c08301113f0c30a
Brian Morgan <brianwmorgan317@gmail.com>
2022-06-09 12:43:59 -0400

    first commit

---
+++ src/UserListAndPostTitlesApp.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function UserListAndPostTitlesApp() {
+  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [showUserPosts, setShowUserPosts] = useState({});
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/users`
+      );
+      setUsers(await response.json());
+    }
+    loadUsers();
+  }, []);
+
+  useEffect(() => {
+    async function loadPosts() {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/posts`
+      );
+      setPosts(await response.json());
+    }
+    loadPosts();
+  }, []);
+
+  const handleUserClick = (event) => {
+    event.preventDefault();
+    const userName = event.target.innerText;
+    const userPreviousValue = showUserPosts[userName]
+    setShowUserPosts({
+      ...showUserPosts,
+      [userName]: !userPreviousValue
+    });
+  };
+
+  const userList = users.slice(0,5).map((user) => {
+    return (
+      <li key={user.id}>
+        <a href="#" onClick={handleUserClick}>
+          {user.name}
+        </a>
+        {showUserPosts[user.name] ? posts.map((post) => {
+          if (post.userId === user.id) {
+            return <li key={post.id}>{post.title}</li>;
+          }
+        }) : null}
+      </li>
+    );
+  });
+
+
+  return (
+    <div>
+      <h1>Frontend Practice - React App</h1>
+      <h2>Goals of this app:</h2>
+      <p>1 - Use JSON Placeholder to load a list of the first five users</p>
+      <p>
+        2 - Clicking a user displays their first three post titles beneath their
+        name
+      </p>
+      <p>3 - Clicking a user a second time hides the post titles</p>
+      <ul>{userList}</ul>
+    </div>
+  );
+}
+
+export default UserListAndPostTitlesApp;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import UserListAndPostTitlesApp from "./UserListAndPostTitlesApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UserListAndPostTitlesApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
